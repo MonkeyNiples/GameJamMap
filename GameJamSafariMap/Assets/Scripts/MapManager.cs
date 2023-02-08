9d4619fbd7efa9d99c096cdfcd914401d017a5b4
MonkeyNiples <62117890+MonkeyNiples@users.noreply.github.com>
2023-02-08 20:45:51 +0100

    fix for everything

--- GameJamSafariMap/Assets/Scripts/MapManager.cs
+++ GameJamSafariMap/Assets/Scripts/MapManager.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
-using TreeEditor;
 using Unity.VisualScripting.Antlr3.Runtime.Tree;
 using UnityEngine;
 public class MapManager : MonoBehaviour
